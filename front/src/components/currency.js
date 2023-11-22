5aa7728e41786fdb7d24f575ad3624a23ca65e00
Artyom <keloss94@mail.ru>
2023-11-23 00:48:43 +0700

    remove all

--- front/src/components/currency.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const Currency = {
-    async getAll() {
-        const response = await axios.get('https://www.cbr-xml-daily.ru/latest.js')
-        return response.data
-    }
-}
